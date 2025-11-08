c64f662fa4d7d401ef347406ee96ca31018e6349
Giapthe <giapthuy2002@gmail.com>
2025-11-08 20:31:25 +0700

    create layout for website

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({children}) => {
+  return (
+    <div className="">
+
+      <main className="max-w-[1400px] mx-auto w-full">
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+      
+    </div>
+  );
 };
 
 export default Layout;
